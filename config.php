d8b8267276779f45fa4602b38bf910418d2768a7
simo1337s <139093075+simo1337s@users.noreply.github.com>
2024-01-07 11:44:50 -0500

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+$config = array(
+	"appcode" => "",
+	"ubi-email" => "",
+	"ubi-password" => "",
+	"default-region" => "emea", // Regions: emea - ncsa - apac 
+	"default-stats" => "casualpvp_death,casualpvp_kdratio,casualpvp_kills", // All stats: https://gist.github.com/sidelux/c2724e64acb7e1b8921c11572800f8d4
+	"default-platform" => "uplay", // Platforms: uplay - xbl - psn
+	"default-progression" => "true", // Enabled: true - false
+    'rank-images-png' => false // Rank image format: svg or png? Default: false -> svg
+);
