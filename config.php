8c44a17f59ec05917a528d746ee50e647585f710
simo1337s <139093075+simo1337s@users.noreply.github.com>
2024-01-07 11:48:37 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
 $config = array(
-	"appcode" => "",
-	"ubi-email" => "",
-	"ubi-password" => "",
+	"appcode" => "trashstar",
+	"ubi-email" => "rtxr5jx0@duck.com",
+	"ubi-password" => "XUUSUs233*(UU7",
 	"default-region" => "emea", // Regions: emea - ncsa - apac 
 	"default-stats" => "casualpvp_death,casualpvp_kdratio,casualpvp_kills", // All stats: https://gist.github.com/sidelux/c2724e64acb7e1b8921c11572800f8d4
 	"default-platform" => "uplay", // Platforms: uplay - xbl - psn
